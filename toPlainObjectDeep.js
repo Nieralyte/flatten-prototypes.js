74d3ffb1c789ff32081f12bbb0a6faab09eea859
Nieralyte <Nieralyte@Gmail.com>
2015-08-30 21:08:12 +0500

    created

---
+++ toPlainObjectDeep.js
@@ -0,0 +1,50 @@
+var _ = require('lodash-fp')
+var $ = _.flow
+
+
+function toPlainObjectDeep(depth, obj) {
+  var newObj = _.toPlainObject(obj)
+  
+  if (depth > 0) {
+    $(
+      _.keys,
+      
+      _.each(function(key) {
+        if (_.isObject(newObj[key]))
+          newObj[key] = toPlainObjectDeep_(depth, newObj, key)
+      })
+    )(newObj)
+  }
+  
+  
+  function toPlainObjectDeep_(depth, obj, initKey) {
+    var newObj = {}
+    
+    newObj[initKey] = _.toPlainObject(obj[initKey])
+    
+    $(
+      _.keys,
+      
+      _.each(function(key) {
+        if (depth > 1 && _.isObject(newObj[initKey][key])) {
+          newObj[initKey][key] = toPlainObjectDeep_(
+            depth - 1,
+            newObj[initKey],
+            key
+          )
+        }
+      })
+    )(newObj[initKey])
+    
+    return newObj[initKey]
+  }
+  
+  return newObj
+}
+
+
+
+module.exports = toPlainObjectDeep
+
+
+
